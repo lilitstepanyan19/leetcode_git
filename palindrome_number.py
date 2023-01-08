06a62fdc6b582e944ebb62e1e3724540ce99b214
lilitstepanyan19 <lil-lid@mail.ru>
2023-01-08 16:10:53 +0400

    updated palindrome_number.py

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,12 +1,19 @@
 def bla(x):
     if x < 0:
         return False
+    num = 0
+    orig = x
+    while x:
+        x, d = divmod(x, 10)
+        num = num * 10 + d
+    return num == orig
 
-    x = str(x)
-    if x == x[::-1]:
-        return True
+
+    # x = str(x)
+    # if x == x[::-1]:
+    #     return True
     
-    return False
+    # return False
 
 
 
