17cbc3b3630aba57da2c7bab2323abe3c4cb0999
lilitstepanyan19 <lil-lid@mail.ru>
2023-01-20 13:43:56 +0400

    binary_search_704.py

---
+++ binary_search_704.py
@@ -0,0 +1,21 @@
+def search(nums, target):
+
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] > target:
+            r = mid - 1
+        if nums[mid] < target:
+            l = mid + 1
+    return -1
+
+    # for i in range(len(nums)):
+    #     if target == nums[i]:
+    #         return i
+    # return -1
+
+print(search([-1,0,3,5,9,12], 9))
+print(search([-1,0,3,5,9,12], 2))
