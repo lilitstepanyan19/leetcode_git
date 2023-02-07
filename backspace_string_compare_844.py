b740ab0bf425a00e9f69e04dbbfe9d63bcc05b69
lilitstepanyan19 <lil-lid@mail.ru>
2023-02-07 13:54:59 +0400

    backspace_string_compare_844.py

---
+++ backspace_string_compare_844.py
@@ -0,0 +1,27 @@
+def backspaceCompare(s, t):
+    def x(s):
+        m = []
+        for el in s:
+            if el == '#':
+                if m:
+                    m.pop()
+            else:
+                m.append(el)
+        return ''.join(m)
+    return x(s) == x(t)
+
+    # def x(s):
+    #     m = []
+    #     for el in s:
+    #         if m and el == '#':
+    #             m.pop()
+    #         elif el != '#':
+    #             m.append(el)
+    #     return m    
+    # return x(s) == x(t)
+
+# print(backspaceCompare("ab#c", "ad#c"))
+# print(backspaceCompare("ab##", "c#d#"))
+# print(backspaceCompare("a#c", "b"))
+print(backspaceCompare("oi###mupo##rszty#s#xu###bxx##dqc#gdjz", "oi###mu#ueo##pk#o##rsztu#y#s#xu###bxx##dqc#gz#djz"))
+
