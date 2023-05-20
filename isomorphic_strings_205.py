a05b86d24fa7549f381d92eb4ed3a2521785d339
lilitstepanyan19 <lil-lid@mail.ru>
2023-05-20 21:09:50 +0400

    isomorphic_strings_205.py

---
+++ isomorphic_strings_205.py
@@ -0,0 +1,14 @@
+def isIsomorphic(s, t):
+    return map(s.find,s) == map(t.find,t)
+
+    # for i in range( len(s)):
+    #         if t[i] not in s or s[i] == t[i]:
+    #             s = s.replace(s[i], t[i])   
+    # if s == t:
+    #     return True
+    # return False
+print(isIsomorphic("egg", "add"))
+print(isIsomorphic("foo", "bar"))
+print(isIsomorphic("paper", "title"))
+print(isIsomorphic("babc", "baba"))
+print(isIsomorphic("abab", "baba"))
