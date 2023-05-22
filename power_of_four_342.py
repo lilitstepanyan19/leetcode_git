c038e312a6953691028f41701b26a42575f229a2
lilitstepanyan19 <lil-lid@mail.ru>
2023-05-22 20:07:21 +0400

    power_of_four_342.py

--- power_of_four_342.py
+++ power_of_four_342.py
@@ -1,16 +1,28 @@
 def isPowerOfFour(n):
-    if n < 1:
-            return False
-    if n == 1:
+        # return n & (n-1) == 0 and n % 3 == 1
+    if n == 1 or n == 4:
             return True
+    for i in range(2, int(n / 4)):
+        if 4 ** i == n:
+            return True
+        if 4 ** i > n:
+            return False
+    return False
 
-    return isPowerOfFour(n / 4)
+print(isPowerOfFour(16))
+print(isPowerOfFour(5))
+print(isPowerOfFour(1))
+print(isPowerOfFour(129140162))
+
+#     if n < 1:
+#             return False
+#     if n == 1:
+#             return True
+#     return isPowerOfFour(n / 4)
+
     # for i in range(n):
     #     if 4 ** i == n:
     #         return True
 
     # return False
 
-print(isPowerOfFour(16))
-print(isPowerOfFour(5))
-print(isPowerOfFour(1))
